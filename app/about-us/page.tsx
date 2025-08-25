ddb760a1642a83e8f7cf4cec6b37db3f51f7b053
Dang Viet Quang <dvquang5902@gmail.com>
2025-08-25 14:58:44 +0700

    fix: render years

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -5,6 +5,18 @@ import SubcribeSection from "@/components/Subcribe";
 import Image from "next/image";
 
 function AboutUs() {
+  const years = [
+    { label: 1927, active: true },
+    { label: 1940, active: false },
+    { label: 1957, active: false },
+    { label: 1965, active: false },
+    { label: 1979, active: false },
+    { label: 1990, active: false },
+    { label: 1999, active: false },
+    { label: 2010, active: false },
+    { label: 2024, active: false },
+  ];
+
   return (
     <div className="pt-[130px]">
       <ChildHeaderPage
@@ -15,8 +27,26 @@ function AboutUs() {
       />
       <Divider type="without-logo" />
       <div className="pt-[70px] pb-[100px] container mx-auto">
-        <Image src={svgs.line} alt="line" width={5000} height={1000} />
+        <div className="relative">
+          <Image src={svgs.line} alt="line" width={5000} height={1000} />
+          {/* Years */}
+          <div className="absolute top-[-40px] left-0 right-0 px-[90px] flex justify-between">
+            {years.map((item, index) => (
+              <span
+                key={index}
+                className={`text-[22px] ${
+                  item.active
+                    ? "text-[#DCCA87] font-medium"
+                    : "text-[#969493] font-normal"
+                }`}
+              >
+                {item.label}
+              </span>
+            ))}
+          </div>
+        </div>
 
+        {/* Content */}
         <div className="mt-[70px] flex flex-col gap-[40px]">
           <h3 className="text-white font-semibold text-[50px]">
             Lorem Ipsum is simply dummy text
@@ -30,16 +60,16 @@ function AboutUs() {
             typesetting, remaining essentially unchanged. It was popularised in
             the 1960s with the release of Letraset sheets containing Lorem Ipsum
             passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum. Lorem
-            Ipsum&nbsp;is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book. It has survived
-            not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
+            Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum&nbsp;
+            is simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry&apos;s standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
           </p>
           <p className="text-[#969493] text-[16px] font-normal leading-[28px]">
             Lorem Ipsum is simply dummy text of the printing and typesetting
